680fe3383c2c53430b07ab6bab12d3e3f1bf8c07
KAprachir <kaprachir23@gmail.com>
2026-05-26 19:47:39 +0600

    fix: add client URL to CORS configuration for deployment

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       process.env.CLIENT_URL || "http://localhost:3000",
+      "https://assignment-9-client-prachir.vercel.app",
     ],
     credentials: true,
   }),
